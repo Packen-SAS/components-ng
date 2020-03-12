a476267a798216dfc9e25386b851de8b749579fd
Jhampier Giron <jose.giron@packen.co>
2020-03-12 14:45:34 -0500

    Se terminan pruebas unitarias del componente toggle

--- src/app/components/packen-toggle/packen-toggle.component.spec.ts
+++ src/app/components/packen-toggle/packen-toggle.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PackenToggleComponent } from './packen-toggle.component';
+import { ToggleItem } from '../../interfaces/toggle-item';
 
 describe('PackenToggleComponent', () => {
   let component: PackenToggleComponent;
@@ -8,9 +9,9 @@ describe('PackenToggleComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PackenToggleComponent ]
+      declarations: [PackenToggleComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +23,35 @@ describe('PackenToggleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //Styles of toggle
+  it('render classStyleColor when toggle is disabled', () => {
+    var objToggle: ToggleItem = { id: 1, disabled: true, offLabel: "Off", onLabel: 'On', status: 'checked' }
+    expect(component.getClassStylesColor(objToggle)).toEqual('switch__slider--disabled');
+  });
+
+  it('render classStyleColor when toggle is disabled', () => {
+    var objToggle: ToggleItem = { id: 1, disabled: false, offLabel: "Off", onLabel: 'On', status: 'checked' }
+    expect(component.getClassStylesColor(objToggle)).toEqual('switch__slider--default');
+  });
+  //Styles of toggle
+  
+
+  //Change toggle
+  it('render function changeToggle when is disabled', () => {
+    var objToggle: ToggleItem = { id: 1, disabled: true, offLabel: "Off", onLabel: "On", status: 'checked' };
+    expect(component.changeToggle(objToggle));
+  });
+
+  it('render function changeToggle for value status, status is checked and disabled is false', () => {
+    var objToggle: ToggleItem = { id: 1, disabled: false, offLabel: "Off", onLabel: "On", status: 'checked' };
+    expect(component.changeToggle(objToggle));
+  });
+
+  it('render function changeToggle for value status, status is unchecked and disabled is false', () => {
+    var objToggle: ToggleItem = { id: 1, disabled: false, offLabel: 'Off', onLabel: "On", status: 'unchecked' };
+    expect(component.changeToggle(objToggle));
+
+  });
+  //Change toggle
 });
