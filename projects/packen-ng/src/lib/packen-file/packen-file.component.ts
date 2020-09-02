2e4c7f53251d04289845a863681361edebad82b8
german <german@packen.co>
2020-09-02 11:20:07 -0500

    Changing return type to File

--- projects/packen-ng/src/lib/packen-file/packen-file.component.ts
+++ projects/packen-ng/src/lib/packen-file/packen-file.component.ts
@@ -23,7 +23,7 @@ export class PackenFileComponent implements OnInit, OnChanges {
 
   classInput: string = '';
   classInputPrevious: string = '';
-  selectedFile: any;
+  selectedFile: File;
 
   @Output()
   valueChange = new EventEmitter<File>();
@@ -84,17 +84,11 @@ export class PackenFileComponent implements OnInit, OnChanges {
   }
 
   fileChange(event) {
-    const file = event.target.files[0];
+    const file = event.srcElement.files[0];
     if (file) {
+      this.selectedFile = file;
+      this.valueChange.emit(this.selectedFile);
       this.classInput = this.classInputPrevious;
-
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-
-      reader.onloadend = (ev) => {
-        this.selectedFile = ev.target.result;
-        this.valueChange.emit(this.selectedFile);
-      };
     }
   }
 
