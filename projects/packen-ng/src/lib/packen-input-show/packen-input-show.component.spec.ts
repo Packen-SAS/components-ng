636699cc9fc61fe69d0c518219c0601ebe93237b
Jhampier Giron <jose.giron@packen.co>
2020-09-18 16:24:22 -0500

    Remove the focus in test unit

--- projects/packen-ng/src/lib/packen-input-show/packen-input-show.component.spec.ts
+++ projects/packen-ng/src/lib/packen-input-show/packen-input-show.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PackenInputShowComponent } from './packen-input-show.component';
 
-fdescribe('PackenInputShowComponent', () => {
+describe('PackenInputShowComponent', () => {
   let component: PackenInputShowComponent;
   let fixture: ComponentFixture<PackenInputShowComponent>;
 
