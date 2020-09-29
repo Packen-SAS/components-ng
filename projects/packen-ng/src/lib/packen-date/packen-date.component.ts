fb2b087e9545d423bf6a3072122abeada6c1e2b6
Jhampier Giron <jose.giron@packen.co>
2020-09-29 18:29:33 -0500

    Changed the version for upload

--- projects/packen-ng/src/lib/packen-date/packen-date.component.ts
+++ projects/packen-ng/src/lib/packen-date/packen-date.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'lib-packen-date',
