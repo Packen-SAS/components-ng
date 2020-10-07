0aa461bfb086c70428d9312c15a7a18d98d6ad5c
german <german@packen.co>
2020-10-07 17:21:44 -0500

    Fixing packen pin map

--- projects/packen-ng/src/lib/packen-pin-map/packen-pin-map.component.ts
+++ projects/packen-ng/src/lib/packen-pin-map/packen-pin-map.component.ts
@@ -6,95 +6,147 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./packen-pin-map.component.scss']
 })
 export class PackenPinMapComponent implements OnInit {
+
   @Input() icon: string = '';
+  @Input() letter: string = '';
   @Input() label: string = '';
   @Input() direction: string = '';
-  @Input() iconPosition: string = 'right';
-  @Input() typeIcon: string = 'icon';
+
   @Input() type: string = 'default';
-  @Input() dotPosition: string = '';
-  @Input() letter: string = '';
+  @Input() typeIcon: string = 'icon';
+
+  @Input() dotPosition: PointPosition = 'none';
+  @Input() iconPosition: IconPosition = 'right';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  getClassTypeContentDirection = (type) => {
-    if (type === 'active') {
-      return ContentDirection.active;
-    } else if (type === 'refresh') {
-      return ContentDirection.default;
-    } else {
-      return ContentDirection.default;
+  getClassIconBox() {
+    let styles = '';
+    if (this.type === TypeColor.refresh) {
+      styles += ColorContentIcon.refresh;
     }
+
+    if (this.dotPosition === TypePointPosition.bottom) {
+      styles += ' ' + PositionContentIcon.top;
+    } else if (this.dotPosition === TypePointPosition.top) {
+      styles += ' ' + PositionContentIcon.bottom;
+    } else if (this.dotPosition === TypePointPosition.right) {
+      styles += ' ' + PositionContentIcon.left;
+    } else {
+      styles += ' ' + PositionContentIcon.right;
+    }
+
+    return styles;
   }
 
-  getClassLabel = (type) => {
-    if (type === 'active') {
+  getColorIcon() {
+    if (this.type === TypeColor.refresh) {
+      return IconColorStyles.refresh;
+    }
+    return IconColorStyles.default;
+  }
+
+  getClassAddress() {
+    let styles = '';
+    if (this.type === TypeColor.active) {
+      styles += ContentColor.active;
+    } else {
+      styles += ContentColor.default;
+    }
+
+    if (this.iconPosition === TypeIconPosition.right) {
+      styles += ' ' + ContentDirection.left;
+    } else {
+      styles += ' ' + ContentDirection.right;
+    }
+
+    return styles;
+  }
+
+  getClassLabel() {
+    if (this.type === TypeColor.active) {
       return ClassLabelStyles.active;
     } else {
       return ClassLabelStyles.default;
     }
   }
 
-  getClassPointPosition = (position) => {
+  getClassPointPosition(position) {
     switch (position) {
-      case 'left':
+      case TypePointPosition.left:
         return PositionPointStyle.left;
-      case 'top':
+      case TypePointPosition.top:
         return PositionPointStyle.top;
-      case 'down':
-        return PositionPointStyle.down;
-      case 'right':
+      case TypePointPosition.bottom:
+        return PositionPointStyle.bottom;
+      case TypePointPosition.right:
         return PositionPointStyle.right;
-      case 'none':
-        return PositionPointStyle.none;
       default:
         return PositionPointStyle.none;
     }
   }
 
-  getColorContentIcon = (type) => {
-    if (type === 'refresh') {
-      return ColorContentIcon.refresh;
-    }
-  }
-
-  getColorIcon = (type) => {
-    if (type === 'refresh') {
-      return IconColorStyles.refresh;
-    }
-    return IconColorStyles.default;
-  }
-
 }
 class PositionPointStyle {
-  static readonly left = 'content__iconAndPoint__point--left';
-  static readonly right = 'content__iconAndPoint__point--right';
-  static readonly top = 'content__iconAndPoint__point--top';
-  static readonly down = 'content__iconAndPoint__point--down';
-  static readonly none = 'content__iconAndPoint__point--none';
+  static readonly left = 'content__point--left';
+  static readonly right = 'content__point--right';
+  static readonly top = 'content__point--top';
+  static readonly bottom = 'content__point--bottom';
+  static readonly none = 'content__point--none';
+}
+
+class ContentColor {
+  static readonly active = 'content__address--active';
+  static readonly default = 'content__address--default';
 }
 
 class ContentDirection {
-  static readonly active = 'content__direction--active';
-  static readonly default = 'content__direction--default';
+  static readonly right = 'content__address--right';
+  static readonly left = 'content__address--left';
 }
 
 class ClassLabelStyles {
-  static readonly active = 'content__direction__text--labelActive';
-  static readonly default = 'content__direction__text--labelDefault';
+  static readonly active = 'content__address__text--labelActive';
+  static readonly default = 'content__address__text--labelDefault';
 }
 
 class IconColorStyles {
-  static readonly refresh = 'content__iconAndPoint__contentIcon__text--icon--refresh';
-  static readonly default = 'content__iconAndPoint__contentIcon__text--icon';
+  static readonly refresh = 'content__iconbox--icon--refresh';
+  static readonly default = 'content__iconbox--icon';
 }
 
 class ColorContentIcon {
-  static readonly refresh = 'content__iconAndPoint__contentIcon--refresh';
-
+  static readonly refresh = 'content__iconbox--refresh';
 }
 
-type PositionsPoint = 'left' | 'top' | 'right' | 'down' | 'none';
-type TypeDirection = 'active' | 'refresh' | 'default';
+class PositionContentIcon {
+  static readonly top = 'content__iconbox--top';
+  static readonly bottom = 'content__iconbox--bottom';
+  static readonly right = 'content__iconbox--right';
+  static readonly left = 'content__iconbox--left';
+}
+
+class TypeColor {
+  static readonly active = 'active';
+  static readonly refresh = 'refresh';
+  static readonly default = 'default';
+}
+
+class TypeIconPosition {
+  static readonly left = 'left';
+  static readonly right = 'right';
+}
+
+class TypePointPosition {
+  static readonly left = 'left';
+  static readonly right = 'right';
+  static readonly top = 'top';
+  static readonly bottom = 'bottom';
+  static readonly none = 'none';
+}
+
+type PointPosition = 'left' | 'top' | 'right' | 'bottom' | 'none';
+type IconPosition = 'left' | 'right';
